17e8b9dfa2880d60215369b67823ef145732d020
Peter Polidoro <peterpolidoro@gmail.com>
2015-07-02 16:38:59 -0400

    Added BioshakeError to import list.

--- bioshake_device/__init__.py
+++ bioshake_device/__init__.py
@@ -4,4 +4,4 @@ BioshakeDevice, which contains an instance of
 serial\_device2.SerialDevice and adds methods to it to interface to
 Q.instruments BioShake devices.
 '''
-from bioshake_device import BioshakeDevice, BioshakeDevices, find_bioshake_device_ports, find_bioshake_device_port, __version__
+from bioshake_device import BioshakeDevice, BioshakeDevices, BioshakeError, find_bioshake_device_ports, find_bioshake_device_port, __version__
