ddc7f9a5416286e8aec44956ac277530c7b8a438
Peter Polidoro <peterpolidoro@gmail.com>
2015-08-12 10:27:55 -0400

    Updated homepage url.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/JaneliaSciComp/bioshake_device_python',
+    url='https://github.com/janelia-pypi/bioshake_device_python',
 
     # Author details
     author='Peter Polidoro',
